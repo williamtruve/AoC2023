979fb5d9193520c755028f681bfbbbc9cc1ad444
williamtruve <williamtruve@gmail.com>
2023-12-03 17:23:49 +0100

    day3 + utils

---
+++ aocutils.py
@@ -0,0 +1,52 @@
+def get_neighbors(row, col, grid):
+    neighbors = []
+    if row > 0:
+        neighbors.append((row - 1, col))  # Up
+    if col < len(grid[0]) - 1:
+        neighbors.append((row, col + 1))  # Right
+    if row < len(grid) - 1:
+        neighbors.append((row + 1, col))  # Down
+    if col > 0:
+        neighbors.append((row, col - 1))  # Left
+    return neighbors
+
+
+def get_neighbors_with_diagonal(row, col, grid):
+    neighbors = get_neighbors(row, col, grid)
+    rows, cols = len(grid), len(grid[0])
+
+    if row > 0 and col > 0:
+        neighbors.append((row - 1, col - 1))  # Up-Left
+    if row > 0 and col < cols - 1:
+        neighbors.append((row - 1, col + 1))  # Up-Right
+    if row < rows - 1 and col > 0:
+        neighbors.append((row + 1, col - 1))  # Down-Left
+    if row < rows - 1 and col < cols - 1:
+        neighbors.append((row + 1, col + 1))  # Down-Right
+
+    return neighbors
+
+
+def get_neighbors_wrap_around(row, col, grid):
+    neighbors = []
+    rows, cols = len(grid), len(grid[0])
+
+    neighbors.append(((row - 1 + rows) % rows, col))  # Up with wrap around
+    neighbors.append((row, (col + 1) % cols))  # Right with wrap around
+    neighbors.append(((row + 1) % rows, col))  # Down with wrap around
+    neighbors.append((row, (col - 1 + cols) % cols))  # Left with wrap around
+
+    return neighbors
+
+
+def get_neighbors_with_diagonal_wrap_around(row, col, grid):
+    neighbors = get_neighbors_wrap_around(row, col, grid)
+    rows, cols = len(grid), len(grid[0])
+
+    neighbors.extend([
+        ((row - 1 + rows) % rows, (col - 1 + cols) % cols),  # Up-Left with wrap around
+        ((row - 1 + rows) % rows, (col + 1) % cols),  # Up-Right with wrap around
+        ((row + 1) % rows, (col - 1 + cols) % cols),  # Down-Left with wrap around
+        ((row + 1) % rows, (col + 1) % cols),  # Down-Right with wrap around
+    ])
+    return neighbors
---
+++ day3/day3part1.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+from aocd import get_data, submit
+import re
+import numpy as np
+from aocutils import *
+data = get_data(day=3, year=2023)
+#data = open("day3/test.txt", "r").read()
+
+data = data.splitlines()
+matrix = []
+for d in data:
+    matrix.append(d.strip())
+
+matCopy = matrix.copy()
+total_sum = 0
+
+#Part 1 loop
+pattern = r"\d+"
+for rx, row in enumerate(matrix):
+    numbers = re.finditer(pattern, matCopy[rx])
+    for num in numbers:
+        found = False
+        indexSpan = range(num.start(), num.end())
+        number = num.group()
+        for x in indexSpan:
+            neighbours = get_neighbors_with_diagonal(row = rx, col = x, grid=matrix)
+            for n in neighbours:
+                symbol = re.findall(r"[^\d.]", matrix[n[0]][n[1]])
+                if symbol:
+                    total_sum += int(number)
+                    found = True
+                    break
+            if found:
+                break
+
+print(total_sum)
+
+
+#Part 2 loop
+pattern = r"\d+"
+gears = defaultdict(list)
+for rx, row in enumerate(matrix):
+    numbers = re.finditer(pattern, matCopy[rx])
+    for num in numbers:
+        found = False
+        indexSpan = range(num.start(), num.end())
+        number = int(num.group())
+        for x in indexSpan:
+            neighbours = get_neighbors_with_diagonal(row = rx, col = x, grid=matrix)
+            for n in neighbours:
+                symbol = re.findall(r"\*", matrix[n[0]][n[1]])
+                if symbol:
+                    gears[n].append(number)
+                    found = True
+                    break
+            if found:
+                break
+
+tot_sum = 0
+for key, value in gears.items():
+    if len(value) == 2:
+        tot_sum += (value[0]*value[1])
+print(tot_sum)
